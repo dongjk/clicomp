47b2529125d98e0451152fe3b57ff288ea3c485e
dongjk <jiankang.jiankang@gmail.com>
2026-03-26 11:13:53 +0800

    remove channel

--- clicomp/channels/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Chat channels module with plugin architecture."""
-
-from clicomp.channels.base import BaseChannel
-from clicomp.channels.manager import ChannelManager
-
-__all__ = ["BaseChannel", "ChannelManager"]
